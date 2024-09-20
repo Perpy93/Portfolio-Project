059eb4cde5cc5d1ee9a3d5d84d78a7f10bf913c7
Perpy <Perpetualchioma00@gmail.com>
2024-09-19 23:23:58 -0700

    completed all api endpoints

--- Back-end/routes/task-route.js
+++ Back-end/routes/task-route.js
@@ -1,11 +1,63 @@
 const router = require('express').Router();
+const Task = require("../models/task-model")
 
 
-router.get("/todos", (req, res)=>{res.send("hello")});
-router.post("/todos", (req, res)=>{res.send("hello")});
-router.get("/todos/:todoId", (req, res)=>{res.send("hello")});
-router.put("/todos/:todoId", (req, res)=>{res.send("hello")});
-router.delete("/todos/:todoId", (req, res)=>{res.send("hello")});
+router.get("/todos", async (req, res)=>{
+
+    try {
+        const tasks = await Task.find();
+        res.status(200).json({data: tasks});
+    } catch (error) {
+        res.status(400).json({error})
+    }    
+});
+
+
+router.post("/todos", async (req, res)=>{
+    try {
+        const task = await Task.create({task: req.body.task});
+        res.status(200).json({data: task});
+    } catch (error) {
+        res.status(400).json({error})
+    }  
+});
+
+
+router.get("/todos/:todoId", async (req, res)=>{
+    try {
+        const task = await Task.findOne({_id: req.params.todoId});
+        res.status(200).json({data: task});
+    } catch (error) {
+        res.status(400).json({error})
+    }  
+});
+
+
+router.put("/todos/:todoId", async (req, res)=>{
+    try {
+        const payload = {};
+        if (req.body.task) {
+            payload["task"] =  req.body.task;
+        }
+        if (req.body.isCompleted) {
+            payload["isCompleted"] =  req.body.isCompleted;
+        }
+        const task = await Task.findOneAndUpdate({_id: req.params.todoId}, payload);
+        res.status(200).json({data: task});
+    } catch (error) {
+        res.status(400).json({error});
+    }  
+});
+
+
+router.delete("/todos/:todoId", async (req, res)=>{
+    try {
+        const task = await Task.findOneAndDelete({_id: req.params.todoId});
+        res.status(200).json({data: task});
+    } catch (error) {
+        res.status(400).json({error})
+    }  
+});
 
 
 module.exports = router;
